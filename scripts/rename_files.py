ace4e733c6e17f4770046cbf5080ca9cacba00ba
LeZelote <jean99yves@gmail.com>
2025-05-15 20:55:50 +0000

    Blog actualisé

--- scripts/rename_files.py
+++
@@ -1,9 +0,0 @@
-# scripts/rename_files.py
-import os
-
-dossier = input("Entrez le chemin du dossier : ")
-for i, fichier in enumerate(os.listdir(dossier), 1):
-    ancien = os.path.join(dossier, fichier)
-    nouveau = os.path.join(dossier, f"fichier_{i}{os.path.splitext(fichier)[1]}")
-    os.rename(ancien, nouveau)
-print("Renommage terminé !")
