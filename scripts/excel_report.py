0fbb45702c1a55aa445d21cfaed54fa6a3dc776c
LeZelote <jean99yves@gmail.com>
2025-05-15 20:45:36 +0000

    Scripts actualisés

--- scripts/excel_report.py
+++
@@ -1,6 +0,0 @@
-# scripts/excel_report.py
-import pandas as pd
-df = pd.read_csv("ventes.csv")
-summary = df.groupby("region")["ventes"].sum().reset_index()
-summary.to_excel("rapport_ventes.xlsx")
-print("Rapport généré !")
